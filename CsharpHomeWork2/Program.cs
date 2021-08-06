e6bf85ce7bd542dd385012d18a78b841d668cb99
venbl4 <lebmax2@gmail.com>
2021-08-06 12:37:51 +0700

    Добавьте файлы проекта.

---
+++ CsharpHomeWork2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CsharpHomeWork2
+{
+    class Program
+    {
+    static int MinNum()
+        {
+            Console.Write("Введите три целых числа:\n a= ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b= ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("c= ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int min = a;
+            if (b<min) min = b;
+            if (c<min) min = c;   
+            return min;
+         }
+    static void Main(string[] args)
+        {
+            Console.WriteLine($"Наименьшее из трех введенных чисел равно {MinNum()}");
+            Console.ReadLine();
+        }
+    }
+}
